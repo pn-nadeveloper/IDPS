e8ff13228976d6612f6176b575bd2b2c3b83033f
pn-nadeveloper <pn_nadeveloper@daum.net>
2026-05-09 22:24:11 +0900

    .gitignore 파일 추가

---
+++ modules/db.js
@@ -0,0 +1,6 @@
+import { createClinet } from '@supabase/supabase-js';
+
+const supabaseUrl = process.env.SUPABASE_URL;
+const supabaseKey = process.env.SUPABASE_KEY;
+
+export const supabase = createClinet(supabaseUrl, supabaseKey);
